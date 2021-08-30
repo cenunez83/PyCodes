283c84e90543a06071191469d2c1df23d271eefe
cenunez83 <cesare.nunez@gmail.com>
2021-08-30 13:53:27 -0400

    Create 1.8_Adding_Comments_to_Scripts.py

---
+++ UPenn_Course_Review/1.8_Adding_Comments_to_Scripts.py
@@ -0,0 +1,9 @@
+#Here's an example program to greet a user.
+
+"""This code will:
+Prompt for your name
+Set the 'name' variable
+Print a message"""
+
+name = input("What is your name?")
+print("Hello {}".format(name)) #prints message with user's name.
