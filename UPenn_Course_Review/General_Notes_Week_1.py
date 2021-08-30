c02b7509d0cd9248c41ff1c018af4372d79f6a25
cenunez83 <cesare.nunez@gmail.com>
2021-08-30 14:31:33 -0400

    Create General_Notes_Week_1.py

---
+++ UPenn_Course_Review/General_Notes_Week_1.py
@@ -0,0 +1,39 @@
+"""Assigning a Variable 
+
+You can increment a variable using += and decrement a variable using -+."""
+
+#Example of Increment/Decrement 
+ 
+x = 2
+y = 3
+print(x) 
+print(y)
+
+x = y 
+y = x + 3
+x += 1
+y -= 1
+print(x)
+print(y)
+
+"""Boolean Operators
+
+There are three logical operators that are used to compare values, all of which
+come down to true or false."""
+
+#Example - AND Operator evaluates if two expressions are true or false.
+
+x = 5
+print(x > 3 and x < 5) #Prints False
+print(3 < x < 5) #this is another way of printing the line above; also prints false.
+
+#Example - OR Operator evaluates if at least one expression is true. 
+y = 3
+print(y < 3 or y == 3) #Prints True
+print(y <= 3) #also prints true
+
+#Example - NOT Operator returns the opposite of a value.
+x = 6
+y = 5
+z = (y <= x)
+print(not z) #prints the opposite of res, i.e., changes True to False or False to True. 
