6a68ddb83e4abfaf245d161c4087c6b97882b6cc
cenunez83 <cesare.nunez@gmail.com>
2021-09-04 23:52:19 -0400

    Create 1.9_Code_Along_Cats_and_Dogs.py

---
+++ UPenn_Course_Review/1.9_Code_Along_Cats_and_Dogs.py
@@ -0,0 +1,12 @@
+"""Week 1 Code Along: Cats & Dogs
+
+1. Set a variable x to 'cats'
+2. Set a variable y to 'dogs'
+3. Referencing the variables above, set a new variable s to "It's raining cats and dogs!"
+"""
+
+x = 'cats' #1
+y = 'dogs' #2
+
+s = "It's raining " + x + " and " + y + " ! "
+print(s) 
