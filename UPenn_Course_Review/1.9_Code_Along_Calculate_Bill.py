4538c5ccf85a35f85cea1cec5e27c28509185186
cenunez83 <cesare.nunez@gmail.com>
2021-09-05 17:11:33 -0400

    Update 1.9_Code_Along_Calculate_Bill.py

--- UPenn_Course_Review/1.9_Code_Along_Calculate_Bill.py
+++ UPenn_Course_Review/1.9_Code_Along_Calculate_Bill.py
@@ -30,3 +30,23 @@ total = str(round(total, 2))
 #print the final bill
 print("The total bill is $" + total)
 
+#OR you can leave the total as a float:
+
+bill = float(input("What is the meal amount?"))
+tax = float(input("What is the sales tax (as a percentage)?"))
+tip = float(input("What tip amount would you like to leave (as a percentage)?"))
+
+#Calculate and add tax
+tax_amount = (bill * tax) / 100
+
+#Calculate and add tip 
+tip_amount = ((bill + tax_amount) * tip) / 100
+
+#calculate total bill
+total = bill + tax_amount + tip_amount 
+total = round(total, 2)
+
+
+#print the final bill
+print("The total bill is $" , total) #this prints a space between the dollar sign and the value. 
+print("The total bill is $" , total, sep = '') #this removes the space.
