7430f39bbbcd229ed3c42ad2dfbd979990bb01c9
cenunez83 <cesare.nunez@gmail.com>
2021-09-05 00:10:34 -0400

    Update 1.9_Code_Along_Cats_and_Dogs.py

--- UPenn_Course_Review/1.9_Code_Along_Cats_and_Dogs.py
+++ UPenn_Course_Review/1.9_Code_Along_Cats_and_Dogs.py
@@ -2,11 +2,12 @@
 
 1. Set a variable x to 'cats'
 2. Set a variable y to 'dogs'
-3. Referencing the variables above, set a new variable s to "It's raining cats and dogs!"
+3. Referencing tshe variables above, set a new variable s to "It's raining cats and dogs!"
 """
 
 x = 'cats' #1
 y = 'dogs' #2
 
-s = "It's raining " + x + " and " + y + " ! "
+s = "It's raining " + x + " and " + y + "!"
 print(s) 
+
